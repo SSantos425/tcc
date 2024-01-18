c27d155ac071720b7e4c589530d5d93dc84396bf
SSantos425 <samuel.tri@hotmail.com>
2024-01-18 10:24:05 -0300

    compra

--- app/controllers/compras_controller.rb
+++ app/controllers/compras_controller.rb
@@ -4,5 +4,33 @@ class ComprasController < ApplicationController
   end
 
   def new
+    @compra = Compra.new
   end
+
+  def create
+    @compra = Compra.new(compra_params)
+
+    if @compra.save
+        redirect_to compras_path
+    else
+        render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+    def compra_params 
+      params.require(:compra).permit(
+      :fornecedor_id,
+      :numero_nota,
+      :serie,
+      :data_emissao,
+      :data_recebimento,
+      :total_nota,
+      :total_produtos,
+      :desconto,
+      :acrescimos,
+      :frete)
+    end
+
 end
